3ca460e73e0e6a4e2bf770eb4741b4442210740a
aswinkumar2706 <aswinkumar2706@gmail.com>
2022-01-25 14:41:27 +0530

    My first commit

---
+++ src/Collection/Queue.java
@@ -0,0 +1,19 @@
+package Collection;
+
+import java.util.*;
+
+
+public class Queue {
+	
+		  public static void main(String args[])
+		    {
+		        Queue<String> pq = new PriorityQueue<>();
+		 
+		        pq.add("Geeks");
+		        pq.add("For");
+		        pq.add("Geeks");
+		 
+		        System.out.println(pq);
+		    }
+	}
+
